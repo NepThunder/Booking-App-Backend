8a95789da0d56d0bc680d00b0268f4d0b698a123
Tek N. Chaudhary <tekchau21@gmail.com>
2023-03-29 10:44:42 +0530

    modified:   utils/dbConnect.js

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose');
 mongoose.set('strictQuery', false);
 const Connection=()=>{
-mongoose.connect(process.env.mongo_URL,{useUnifiedTopology: true, useNewUrlParser: true }).then(() => console.log('Database Connected!'))
+mongoose.connect(${process.env.mongo_URL},{useUnifiedTopology: true, useNewUrlParser: true }).then(() => console.log('Database Connected!'))
 .catch(err => console.log(err));
 }
 module.exports=Connection;
